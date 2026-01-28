a89bf64e9e1569d4a935de7be2681104f5edae2a
NOURA EDIMA MBOUTTO <nouraedimamboutto@gmail.com>
2026-01-28 12:33:05 +0100

    structure du projet

---
+++ src/app/pages/liste-projets/liste-projets.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-projets',
+  imports: [],
+  templateUrl: './liste-projets.html',
+  styleUrl: './liste-projets.scss',
+})
+export class ListeProjets {
+
+}
